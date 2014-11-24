d50ca18c59f2eec11c281e69cd2cdee212fb7a5e
jjog1 <gibbons.jago@gmail.com>
2014-11-24 21:21:53 +0000

    Changed to use bufio
    Changed to use bufio so new lines are read in as a single string

--- Client/main.go
+++ Client/main.go
@@ -1,59 +1,57 @@
 package main
 
-import ( "fmt"
-"net"
-"encoding/gob")
+import (
+	"bufio"
+	"encoding/gob"
+	"fmt"
+	"net"
+	"os"
+)
 
-type Person struct{
+type Person struct {
 	Name string
-	Age int
+	Age  int
 }
-
 
 func handleRequest(conn net.Conn) {
-  var msg string
-  fmt.Println("handles start")
-  dec:=gob.NewDecoder(conn)
-  for msg != "End"{
-  err := dec.Decode(&msg)
-  
-  if err!= nil{
-	fmt.Println(err)
-	return
-  }else{
-  fmt.Println("Recieved", msg)}
-  }
-  
-  conn.Close()
+	var msg string
+	fmt.Println("handles start")
+	dec := gob.NewDecoder(conn)
+	for msg != "End" {
+		err := dec.Decode(&msg)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		} else {
+			fmt.Println("Recieved", msg)
+		}
+	}
+
+	conn.Close()
 }
 
-func main(){
+func main() {
 	conn, _ := net.Dial("tcp", "127.0.0.1:3333")
 	defer conn.Close()
+	reader := bufio.NewReader(os.Stdin)
 	go handleRequest(conn)
 
-	for{
-		var msg string
+	for {
+		msg, _ := reader.ReadString('\n')
 
-		length,_ := fmt.Scanf("%s",&msg)
-
-		fmt.Println("Length: ",length)
-		person:= Person{msg, 20}
-		
-		fmt.Println("Client",person)
-		
 		//Scanf seems to be reading a second blank line
-		//for every input. 
-		if length > 0{
-			// Creating a new encoder doesn't seem the best 
-			// way to do this. 
-			err := gob.NewEncoder(conn).Encode(&person)
-				
-			if err != nil{
-				fmt.Println("2",err)
-			}
-			
-			length = 0
+		//for every input.
+
+		person := Person{msg, 20}
+
+		// Creating a new encoder doesn't seem the best
+		// way to do this.
+		err := gob.NewEncoder(conn).Encode(&person)
+
+		if err != nil {
+			fmt.Println("2", err)
 		}
+
 	}
 }
